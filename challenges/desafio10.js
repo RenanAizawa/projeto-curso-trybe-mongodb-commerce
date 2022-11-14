0c6e4426d164dc716e5ceb320ab034d4d9d47e65
Renan Aizawa Alves Mascarenhas <renanaizawa@outlook.com>
2022-11-14 18:50:26 -0400

    desafio10, 3 tent

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -1,7 +1,7 @@
 db.produtos.find(
     { $and: [
-        { "valoresNutricionais.0.quantidade": { $gte: 30 } },
-        { "valoresNutricionais.0.quantidade": { $lte: 40 } }] },
+        { "valoresNutricionais.3.quantidade": { $gte: 30 } },
+        { "valoresNutricionais.3.quantidade": { $lte: 40 } }] },
     {
       _id: 0,
       nome: 1,
